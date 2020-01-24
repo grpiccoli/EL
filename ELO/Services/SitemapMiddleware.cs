6513f9765343304a3e798fba123abcea8a1292c6
grpltda <grpiccoli@gmail.com>
2020-01-24 23:32:37 +1300

    +minified js

--- ELO/Services/SitemapMiddleware.cs
+++ ELO/Services/SitemapMiddleware.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
